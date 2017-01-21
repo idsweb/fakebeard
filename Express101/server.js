2a5fe14ce687c852aa3012fd3834326d39847d13
Ian Stafford <Ian Stafford>
2017-01-21 09:18:39 +0000

    Added JSON to express server

--- Express101/server.js
+++ Express101/server.js
@@ -12,6 +12,36 @@ app.get('/', function(req, res){
     res.send('welocome to the index page...');
 });
 
+app.get('/users/:userid', function (req, res) {
+    res.send(req.params); //{"userid":"ian"}
+});
+
+app.get('/api/users',function (req, res) {
+    res.he
+    res.json({
+        users: [
+            {
+                name:'Ian'
+            },
+            {
+                name:'Rod'
+            },
+            {
+                name:'Paul'
+            }]
+        }
+    );
+})
+
+app.get('/api/users/:userid', function (req, res) {
+    res.json({
+        name:'Ian',
+        link:req.url,
+        dob:'05/06/19**'
+    })
+});
+
+/* Note: you need to add these in order, least specific last*/
 app.get('*', function (req,res) {  
     res.status(404).send('These arent the files your looking for');
 });
